80a507dbcf7d2e2495437e462bcb25abf4463d85
David Larsson <david.l.larsson.90>
2019-10-16 23:05:09 +0200

    MovieDatabase
    First Commit

---
+++ ClassLibrary/ClassLibrary/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MovieDatabaseApp
+{
+    public class Movie
+    {
+        public string Title { get; }
+        public int Year { get; }
+        Movie(string title, int year)
+        {
+            Title = title;
+            Year = year;
+        }
+    }
+}
---
+++ ClassLibrary/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleSimulationEngine2000;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var input = new TextInput();
+            var gui = new ConsoleGUI() { Input = input };
+            var sim = new MySimulation(gui, input);
+            await gui.Start(sim);
+        }
+        public class MySimulation : Simulation
+        {
+            private RollingDisplay log = new RollingDisplay(0, 0, -1, 12);
+            private BorderedDisplay clockDisplay = new BorderedDisplay(0, 11, 20, 3) { };
+            private BorderedDisplay updateDisplay = new BorderedDisplay(0, 14, 20, 3) { };
+            private BorderedDisplay renderDisplay = new BorderedDisplay(0, 17, 20, 3) { };
+            private BorderedDisplay printDisplay = new BorderedDisplay(0, 20, 20, 3) { };
+            private readonly ConsoleGUI gui;
+            private readonly TextInput input;
+
+            public override List<BaseDisplay> Displays => new List<BaseDisplay>() {
+        log,
+        clockDisplay,
+        updateDisplay,
+        renderDisplay,
+        printDisplay,
+        input.CreateDisplay(0, -3, -1) };
+
+            public MySimulation(ConsoleGUI gui, TextInput input)
+            {
+                this.gui = gui;
+                this.input = input;
+            }
+            public override void PassTime(int deltaTime)
+            {
+                log.Log($"{deltaTime} milliseconds has passed");
+                clockDisplay.Value = DateTime.Now.ToString("HH:mm:ss");
+                updateDisplay.Value = "Update: " + gui.LastUpdateTime.Milliseconds;
+                renderDisplay.Value = "Render: " + gui.BackBufferRenderTime.Milliseconds;
+                printDisplay.Value = "Print: " + gui.ScreenRenderTime.Milliseconds;
+                while (input.HasInput)
+                {
+                    log.Log("Input: " + input.Consume());
+                }
+            }
+        }
+    }
+
+}
